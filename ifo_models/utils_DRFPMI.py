40c26a348f174faaf0ce2566f517f4297bcdf9bb
Keiko Kokeyama <kokeyama@icrr.u-tokyo.ac.jp>
2020-10-09 18:09:12 +0900

    correcting typo and adding HOM model

--- ifo_models/utils_DRFPMI.py
+++ ifo_models/utils_DRFPMI.py
@@ -53,7 +53,7 @@ def model_DRFPMI():
 
 
     # ========= SRC each mirror loss 45ppm =======
-    s sLsr3 15.7396 n4 nsr5
+    s sLsr3 15.7386 n4 nsr5
     bs1 SR3 50e-6 45e-6 0 $a nsr5 nsr4 dump dump
     s sLsr2 11.1115 nsr4 nsr3
     bs1 SR2 500e-6 45e-6 0 $a nsr2 nsr3 nPOS dump
@@ -61,7 +61,12 @@ def model_DRFPMI():
     m1 SRM 0.3 0e-6 0 nsr1 nAS
 
     ## ===== amplitude detectors =====
-
+    ad CR_REFL 0 nREFL
+    ad SB1p_REFL $f1 nREFL
+    ad SB1m_REFL $mf1 nREFL
+    ad SB2p_REFL $f2 nREFL
+    ad SB2m_REFL $mf2 nREFL
+    
     ad CR_POP 0 nPOP
     ad SB1p_POP $f1 nPOP
     ad SB1m_POP $mf1 nPOP
@@ -82,6 +87,126 @@ def model_DRFPMI():
     
     """)
     return base
+
+
+
+def model_HOM_DRFPMI():
+    #
+    # usage: base = utils_DRFPMI.model_DRFPMI()
+    # output is the finesse structure with the DRFPMI configuration
+    #
+    #
+    base = finesse.kat()
+    base.verbose=False
+    base.parse("""
+    # ======== Constants ========================
+    const f1 16.881M
+    const f2 45.0159M
+    const mf1 -16.881M
+    const mf2 -45.0159M
+    const a 0.686
+    const phi_PRM 90
+    const pi 3.1415
+    const phi_SRM 90
+    
+    # ======== Input optics =====================
+    l i1 1 0 n0
+    s s_eo0 0 n0 n_eo1
+    mod eom1 $f1 0.3 1 pm n_eo1 n_eo2
+    s s_eo1 0 n_eo2 n_eo3
+    mod eom2 $f2 0.3 1 pm n_eo3 n_eo4
+    s s_eo2 0 n_eo4 nREFL
+
+    # ======= PRC each mirror loss 45ppm =======
+    # PRC
+    m1 PRM 0.1 45e-6 $phi_PRM nREFL npr1
+    s sLpr1 14.7615 npr1 npr2
+    bs1 PR2 500e-6 45e-6 0 $a npr3 npr2 nPOP nPOP2
+    s sLpr2 11.0661 npr3 npr4
+    bs1 PR3 50e-6 45e-6 0 $a dump dump npr4 npr5
+    s sLpr3 15.7638 npr5 npr6
+
+    # ======= Michelson ========================
+    bs bs1 0.5 0.5 0 45 npr6 n2 n3 n4
+    s lx 26.4018 n3 nx1		#26.6649-thickness*1.754
+    s ly 23.072 n2 ny1		#23.3351-thickness*1.754
+
+
+    # ======== Thick ITMs ======================
+    m IXAR 0     1     0 nx1 nx2
+    s thick_IX 0.15 1.754 nx2 nx3
+    m ITMX 0.996 0.004 0 nx3 nx4
+
+    m IYAR 0     1     0 ny1 ny2
+    s thick_IY 0.15 1.754 ny2 ny3
+    m ITMY 0.996 0.004 90 ny3 ny4
+
+    # ========== Arm     =======================
+    s sx1 3000 nx4 nx5
+    m ETMX 0.999995 5e-06 0 nx5 nTMSX
+
+    s sy1 3000 ny4 ny5
+    m ETMY 0.999995 5e-06 90 ny5 nTMSY
+
+
+    # ========= SRC each mirror loss 45ppm =======
+    s sLsr3 15.7386 n4 nsr5
+    bs1 SR3 50e-6 45e-6 0 $a nsr5 nsr4 dump dump
+    s sLsr2 11.1115 nsr4 nsr3
+    bs1 SR2 500e-6 45e-6 0 $a nsr2 nsr3 nPOS dump
+    s sLsr1 14.7412 nsr2 nsr1
+    m1 SRM 0.3 0e-6 $phi_SRM nsr1 nAS
+
+    # =========  HOM Expansion =======
+    attr PRM Rc -458.1285
+    attr PR2 Rc -3.0764
+    attr PR3 Rc -24.9165
+    attr bs1 Rc 0
+    attr ITMX Rc -1900.   # measured -1904.6
+    attr ETMX Rc 1900.    # measured  1908.24
+    attr ITMY Rc -1900    # measured -1904.4
+    attr ETMY Rc 1900.    # measured  1905.55
+    attr SRM Rc 458.1285
+    attr SR2 Rc -2.9872
+    attr SR3 Rc 24.9165
+    attr IXAR Rc 0
+    attr IYAR Rc 0
+
+    cav XARM ITMX nx4 ETMX nx5
+    cav YARM ITMY ny4 ETMY ny5
+    cav PRX PRM npr1 ITMX nx3
+    cav PRY PRM npr1 ITMY ny3
+    cav SRX SRM nsr1 ITMX nx3
+    cav SRY SRM nsr1 ITMY ny3
+    
+    # ===== amplitude detectors =====
+    ad CR_REFL 0 nREFL
+    ad SB1p_REFL $f1 nREFL
+    ad SB1m_REFL $mf1 nREFL
+    ad SB2p_REFL $f2 nREFL
+    ad SB2m_REFL $mf2 nREFL
+    
+    ad CR_POP 0 nPOP
+    ad SB1p_POP $f1 nPOP
+    ad SB1m_POP $mf1 nPOP
+    ad SB2p_POP $f2 nPOP
+    ad SB2m_POP $mf2 nPOP
+
+    ad CR_TMSX 0_TMSX nTMSX
+    ad SB1p_TMSX $f1 nTMSX
+    ad SB1m_TMSX $mf1 nTMSX
+    ad SB2p_TMSX $f2 nTMSX
+    ad SB2m_TMSX $mf2 nTMSX
+    
+    ad CR_POS 0 nPOS
+    ad SB1p_POS $f1 nPOS
+    ad SB1m_POS $mf1 nPOS
+    ad SB2p_POS $f2 nPOS
+    ad SB2m_POS $mf2 nPOS
+    
+    """)
+    return base
+
 
 
 
