7521bfde3619974b795361a268f2dec5366adbb6
chirose <f20c071k@mail.cc.niigata-u.ac.jp>
2020-12-14 10:54:21 +0900

    add files

---
+++ gui/crosscheck/gui_crosscheck/202012/c_crosscheck_ifo_models_MI.py
@@ -0,0 +1,218 @@
+import pykat
+from pykat import finesse
+import numpy as np
+
+def model_base():
+    #
+    # usage: base = utils_DRFPMI.model_DRFPMI()
+    # output is the finesse structure with the DRFPMI configuration
+    #
+    #
+    base = finesse.kat()
+    base.verbose=False
+    base.parse("""
+    # ======== Constants ========================
+    const f1 16.881M
+    const f2 45.0159M
+    const mf1 -16.881M
+    const mf2 -45.0159M
+    const a 0.686
+    const pi 3.1415
+    const fsb1 16.881M
+    const fsb2 45.0159M
+    # ======== Input optics =====================
+    l i1 1 0 n0
+    s s_eo0 0 n0 n_eo1
+    mod eom1 $f1 0.3 1 pm n_eo1 n_eo2
+    s s_eo1 0 n_eo2 n_eo3
+    mod eom2 $f2 0.3 1 pm n_eo3 n_eo4
+    s s_eo2 0 n_eo4 nREFL
+    # Michelson
+    bs bs1 0.5 0.5 0 45 nREFL n2 n3 nAS
+
+    # X arm
+    s sx1 3000 n2 nx3
+    m ETMX 0.99995 5e-06 0 nx3 nTMSX
+
+    # Y arm
+    s sy1 3000 n3 ny3
+    m ETMY 0.99995 5e-06 90 ny3 nTMSY
+
+    #### ===== amplitude detectors =====
+
+    ad CR_REFL 0 nREFL
+    ad SB1p_REFL $f1 nREFL
+    ad SB1m_REFL $mf1 nREFL
+    ad SB2p_REFL $f2 nREFL
+    ad SB2m_REFL $mf2 nREFL
+
+    ad CR_AS 0 nAS
+    ad SB1p_AS $f1 nAS
+    ad SB1m_AS $mf1 nAS
+    ad SB2p_AS $f2 nAS
+    ad SB2m_AS $mf2 nAS
+
+
+    ad CR_TMSX 0 nTMSX
+    ad SB1p_TMSX $f1 nTMSX
+    ad SB1m_TMSX $mf1 nTMSX
+    ad SB2p_TMSX $f2 nTMSX
+    ad SB2m_TMSX $mf2 nTMSX
+
+    ad CR_TMSY 0 nTMSY
+    ad SB1p_TMSY $f1 nTMSY
+    ad SB1m_TMSY $mf1 nTMSY
+    ad SB2p_TMSY $f2 nTMSY
+    ad SB2m_TMSY $mf2 nTMSY
+
+
+    """)
+
+
+    return base
+
+
+
+def model_HOM_PRFPMI():
+    #
+    # usage: base = utils_DRFPMI.model_DRFPMI()
+    # output is the finesse structure with the DRFPMI configuration
+    #
+    #
+    base = finesse.kat()
+    base.verbose=False
+    base.parse("""
+    
+    # not made script
+    """)
+    return base
+
+
+
+
+def run_sweep(DoF, base):
+    #
+    # usage: model, out = utils_DRFPMI.run_sweep('CARM',base)
+    # out: the computed signals, 
+    # model: updated the finesse structire
+    # model is with the sweep command, such as 'xaxis mirror phi lin -90 90 5000'
+    #
+    model = base.deepcopy()
+    model.parse("""
+    const f1 16.881M
+    const f2 45.0159M
+    pd1 pd1_ASI1 $f1 0 nAS
+    pd1 pd1_ASQ1 $f1 90 nAS
+    pd1 pd1_REFLI1 $f1 0 nREFL
+    pd1 pd1_REFLQ1 $f1 90 nREFL
+    pd1 pd1_TMSXI1 $f1 0 nTMSX
+    pd1 pd1_TMSXQ1 $f1 90 nTMSX
+    pd1 pd1_TMSYI1 $f1 0 nTMSY
+    pd1 pd1_TMSYQ1 $f1 90 nTMSY
+    pd1 pd1_ASI2 $f2 0 nAS
+    pd1 pd1_ASQ2 $f2 90 nAS
+    pd1 pd1_REFLI2 $f2 0 nREFL
+    pd1 pd1_REFLQ2 $f2 90 nREFL
+    pd1 pd1_TMSXI2 $f2 0 nTMSX
+    pd1 pd1_TMSXQ2 $f2 90 nTMSX
+    pd1 pd1_TMSYI2 $f2 0 nTMSY
+    pd1 pd1_TMSYQ2 $f2 90 nTMSY
+
+    yaxis lin abs
+    """)
+
+    if DoF == "MICH":
+        model.parse("""
+        var tuning 0
+        xaxis* tuning phi lin -180 180 5000
+        put* ETMX phi $x1
+        put* ETMY phi $mx1
+        """)
+    out = model.run()
+    return model, out
+
+
+def run_fsig(DoF, base):
+    model = base.deepcopy()
+    model.parse("""
+
+    
+    const fsb1 16.881M
+    const fsb2 45.0159M
+
+    pd2 pd2_ASI1 $fsb1 0 10 nAS
+    pd2 pd2_ASQ1 $fsb1 90 10 nAS
+    pd2 pd2_REFLI1 $fsb1 0 10 nREFL
+    pd2 pd2_REFLQ1 $fsb1 90 10 nREFL
+    pd2 pd2_ASI2 $fsb2 0 10 nAS
+    pd2 pd2_ASQ2 $fsb2 90 10 nAS
+    pd2 pd2_REFLI2 $fsb2 0 10 nREFL
+    pd2 pd2_REFLQ2 $fsb2 90 10 nREFL
+
+    pd2 pd2_TMSXI1 $fsb1 0 10 nTMSX
+    pd2 pd2_TMSXQ1 $fsb1 90 10 nTMSX
+    pd2 pd2_TMSYI1 $fsb1 0 10 nTMSY
+    pd2 pd2_TMSYQ1 $fsb1 90 10 nTMSY
+    pd2 pd2_TMSXI2 $fsb2 0 10 nTMSX
+    pd2 pd2_TMSXQ2 $fsb2 90 10 nTMSX
+    pd2 pd2_TMSYI2 $fsb2 0 10 nTMSY
+    pd2 pd2_TMSYQ2 $fsb2 90 10 nTMSY
+
+    put pd2_ASI1 f2 $x1
+    put pd2_ASQ1 f2 $x1
+    put pd2_REFLI1 f2 $x1
+    put pd2_REFLQ1 f2 $x1
+
+    put pd2_ASI2 f2 $x1
+    put pd2_ASQ2 f2 $x1
+    put pd2_REFLI2 f2 $x1
+    put pd2_REFLQ2 f2 $x1
+
+    put pd2_TMSXQ1 f2 $x1
+    put pd2_TMSXI1 f2 $x1
+    put pd2_TMSYQ1 f2 $x1
+    put pd2_TMSYI1 f2 $x1
+
+    put pd2_TMSXQ2 f2 $x1
+    put pd2_TMSXI2 f2 $x1
+
+    put pd2_TMSYQ2 f2 $x1
+    put pd2_TMSYI2 f2 $x1
+
+    yaxis log abs:deg
+    
+    """)
+
+    ###### choosing DoF ######
+
+    if DoF == "MICH":
+        model.parse("""
+        const fstart 0.0005
+        const fend 500
+        xaxis sig1 f log $fstart $fend 1000
+        fsig sig1 ETMX 10 0
+        fsig sig1 ETMY 10 180
+        """)    
+        
+    out = model.run()
+    
+    return model, out
+
+
+def demod_phase(out, port, SB, N):
+    #
+    # usage: mag, phase = utils_DRFPMI.demod_phase(out, port, SB, N):
+    # out: the computed signals from run_fsig
+    # port: string, either 'REFL','AS','POP' or 'POS'
+    # SB: string, either '1' or '2', indecating f1 or f2 demodulation
+    # N: integer related to the frequency. Should be updated.
+    #
+    #
+    sensI = port+"I"+SB
+    sensQ = port+"Q"+SB
+    I = np.sign(np.sin(np.angle(out[sensI][N])))* np.abs(out[sensI][N])
+    Q = np.sign(np.sin(np.angle(out[sensQ][N])))* np.abs(out[sensQ][N])
+    demod = np.arctan(Q/I)*180/np.pi
+    mag = np.sqrt(I**2 + Q**2)
+    
+    return mag, demod
---
+++ gui/crosscheck/gui_crosscheck/202012/c_crosscheck_mifsim_gui_MI.py
@@ -0,0 +1,286 @@
+
+import pykat
+from pykat import finesse
+import numpy as np
+
+def model_base():
+    #
+    # usage: base = utils_DRFPMI.model_DRFPMI()(修正)
+    # output is the finesse structure with the DRFPMI configuration(修正)
+    #
+    #
+    base = finesse.kat()
+    base.verbose=False
+    base.parse("""
+    l i1 1.0 0.0 0.0 n0
+    s s_eo0 0.0 n0 n_eo1
+    mod eom1 16881000.0 0.3 1 pm 0.0 n_eo1 n_eo2
+    s s_eo1 0.0 n_eo2 n_eo3
+    mod eom2 45015900.0 0.3 1 pm 0.0 n_eo3 n_eo4
+    s s_eo2 0.0 n_eo4 REFL
+    
+    m PRM 0.0 1.0 0.0 REFL npr1
+    s sLpr1 0.0 npr1 npr2
+    bs PR2 1.0 0.0 0.0 0.686 npr3 npr2 POP POP2
+    s sLpr2 0.0 npr3 npr4
+    bs PR3 1.0 0.0 0.0 0.686 dump dump npr4 npr5
+    s sLpr3 0.0 npr5 npr6
+    bs bs1 0.5 0.5 0.0 45.0 npr6 n2 n3 n4
+    s lx 0 n3 nx1
+    s ly 0 n2 ny1
+    m ITMX 0.0 1.0 0.0 nx1 nx2
+    s sx1 3000.0 nx2 nx3
+    m ETMX 0.99995 5e-06 0.0 nx3 TMSX
+    m ITMY 0.0 1.0 90.0 ny1 ny2
+    s sy1 3000.0 ny2 ny3
+    m ETMY 0.99995 5e-06 90.0 ny3 TMSY
+    s sLsr3 0.0 n4 nsr5
+    bs SR3 1.0 0.0 0.0 0.686 nsr5 nsr4 dump dump
+    s sLsr2 0.0 nsr4 nsr3
+    bs SR2 1.0 0.0 0.0 0.686 nsr2 nsr3 POS dump
+    s sLsr1 0.0 nsr2 nsr1
+    m SRM 0.0 1.0 0.0 nsr1 AS
+    
+
+    ad car_ad_REFL 0 REFL
+    ad fsb1_upper_ad_REFL 16881000.0 REFL
+    ad fsb1_lower_ad_REFL -16881000.0 REFL
+    ad fsb2_upper_ad_REFL 45015900.0 REFL
+    ad fsb2_lower_ad_REFL -45015900.0 REFL
+    ad car_ad_AS 0 AS
+    ad fsb1_upper_ad_AS 16881000.0 AS
+    ad fsb1_lower_ad_AS -16881000.0 AS
+    ad fsb2_upper_ad_AS 45015900.0 AS
+    ad fsb2_lower_ad_AS -45015900.0 AS
+    ad car_ad_TMSX 0 TMSX
+    ad fsb1_upper_ad_TMSX 16881000.0 TMSX
+    ad fsb1_lower_ad_TMSX -16881000.0 TMSX
+    ad fsb2_upper_ad_TMSX 45015900.0 TMSX
+    ad fsb2_lower_ad_TMSX -45015900.0 TMSX
+    ad car_ad_TMSY 0 TMSY
+    ad fsb1_upper_ad_TMSY 16881000.0 TMSY
+    ad fsb1_lower_ad_TMSY -16881000.0 TMSY
+    ad fsb2_upper_ad_TMSY 45015900.0 TMSY
+    ad fsb2_lower_ad_TMSY -45015900.0 TMSY
+    ad car_ad_POP 0 POP
+    ad fsb1_upper_ad_POP 16881000.0 POP
+    ad fsb1_lower_ad_POP -16881000.0 POP
+    ad fsb2_upper_ad_POP 45015900.0 POP
+    ad fsb2_lower_ad_POP -45015900.0 POP
+    ad car_ad_POS 0 POS
+    ad fsb1_upper_ad_POS 16881000.0 POS
+    ad fsb1_lower_ad_POS -16881000.0 POS
+    ad fsb2_upper_ad_POS 45015900.0 POS
+    ad fsb2_lower_ad_POS -45015900.0 POS
+    
+    """)
+    
+    return base
+
+
+
+def model_HOM_PRFPMI():
+    #
+    # usage: base = utils_DRFPMI.model_DRFPMI()
+    # output is the finesse structure with the DRFPMI configuration
+    #
+    #
+    base = finesse.kat()
+    base.verbose=False
+    base.parse("""
+    not made script
+    """)
+    return base
+
+
+
+
+def run_sweep(DoF, base):
+    #
+    # usage: model, out = utils_DRFPMI.run_sweep('CARM',base)
+    # out: the computed signals, 
+    # model: updated the finesse structire
+    # model is with the sweep command, such as 'xaxis mirror phi lin -90 90 5000'
+    #
+    model = base.deepcopy()
+    model.parse("""    
+    
+    #=====I phase=====
+    pd1 pd1_fsb1_0_REFL 16881000.0 0.0 REFL
+    pd1 pd1_fsb1_0_AS 16881000.0 0.0 AS
+    pd1 pd1_fsb1_0_TMSX 16881000.0 0.0 TMSX
+    pd1 pd1_fsb1_0_TMSY 16881000.0 0.0 TMSY
+    pd1 pd1_fsb1_0_POP 16881000.0 0.0 POP
+    pd1 pd1_fsb2_0_REFL 45015900.0 0.0 REFL
+    pd1 pd1_fsb2_0_AS 45015900.0 0.0 AS
+    pd1 pd1_fsb2_0_TMSX 45015900.0 0.0 TMSX
+    pd1 pd1_fsb2_0_TMSY 45015900.0 0.0 TMSY
+    pd1 pd1_fsb2_0_POP 45015900.0 0.0 POP
+    
+    #=====Q phase=====
+    pd1 pd1_fsb1_90_REFL 16881000.0 90.0 REFL
+    pd1 pd1_fsb1_90_AS 16881000.0 90.0 AS
+    pd1 pd1_fsb1_90_TMSX 16881000.0 90.0 TMSX
+    pd1 pd1_fsb1_90_TMSY 16881000.0 90.0 TMSY
+    pd1 pd1_fsb1_90_POP 16881000.0 90.0 POP
+    pd1 pd1_fsb2_90_REFL 45015900.0 90.0 REFL
+    pd1 pd1_fsb2_90_AS 45015900.0 90.0 AS
+    pd1 pd1_fsb2_90_TMSX 45015900.0 90.0 TMSX
+    pd1 pd1_fsb2_90_TMSY 45015900.0 90.0 TMSY
+    pd1 pd1_fsb2_90_POP 45015900.0 90.0 POP
+    yaxis lin abs
+    """)
+    
+    if DoF == "CARM":
+        model.parse("""
+        xaxis ETMX phi lin -180 180 5000
+        put* ETMY phi $x1
+        """)
+    elif DoF == "DARM":
+        model.parse("""
+        xaxis ETMX phi lin -180 180 5000
+        put* ETMY phi $mx1
+        
+        """)
+    elif DoF == "MICH":
+        model.parse("""
+        xaxis ETMX phi lin -180 180 5000
+        #put* ITMY phi $mx1
+        #put* ITMX phi $x1
+        put* ETMY phi $mx1
+        """)
+    elif DoF == "PRCL":
+        model.parse("""
+        xaxis PRM phi lin -180 180 5000
+        """)
+    elif DoF == "SRCL":
+        model.parse("""
+        var tuning 0.0
+        xaxis tuning phi lin -180 180 5000
+        put SRM phi $x1
+        """)
+    
+ 
+        
+        
+    out = model.run()
+    return model, out
+
+
+def run_fsig(DoF, base):
+    model = base.deepcopy()
+    model.parse("""
+    
+
+    pd2 pd2_fsb1_0_REFL 16881000.0 0.0 10.0 REFL
+    pd2 pd2_fsb1_90_REFL 16881000.0 90.0 10.0 REFL
+    pd2 pd2_fsb2_0_REFL 45015900.0 0.0 10.0 REFL
+    pd2 pd2_fsb2_90_REFL 45015900.0 90.0 10.0 REFL
+    
+    pd2 pd2_fsb1_0_AS 16881000.0 0.0 10.0 AS
+    pd2 pd2_fsb1_90_AS 16881000.0 90.0 10.0 AS
+    pd2 pd2_fsb2_0_AS 45015900.0 0.0 10.0 AS
+    pd2 pd2_fsb2_90_AS 45015900.0 90.0 10.0 AS
+
+    pd2 pd2_fsb1_0_TMSX 16881000.0 0.0 10.0 TMSX
+    pd2 pd2_fsb1_90_TMSX 16881000.0 90.0 10.0 TMSX
+    pd2 pd2_fsb2_0_TMSX 45015900.0 0.0 10.0 TMSX
+    pd2 pd2_fsb2_90_TMSX 45015900.0 90.0 10.0 TMSX
+
+    pd2 pd2_fsb1_0_TMSY 16881000.0 0.0 10.0 TMSY
+    pd2 pd2_fsb1_90_TMSY 16881000.0 90.0 10.0 TMSY
+    pd2 pd2_fsb2_0_TMSY 45015900.0 0.0 10.0 TMSY
+    pd2 pd2_fsb2_90_TMSY 45015900.0 90.0 10.0 TMSY
+
+    pd2 pd2_fsb1_0_POP 16881000.0 0.0 10.0 POP
+    pd2 pd2_fsb1_90_POP 16881000.0 90.0 10.0 POP
+    pd2 pd2_fsb2_0_POP 45015900.0 0.0 10.0 POP
+    pd2 pd2_fsb2_90_POP 45015900.0 90.0 10.0 POP
+    
+    pd2 pd2_fsb1_0_POS 16881000.0 0.0 10.0 POS
+    pd2 pd2_fsb1_90_POS 16881000.0 90.0 10.0 POS
+    pd2 pd2_fsb2_0_POS 45015900.0 0.0 10.0 POS
+    pd2 pd2_fsb2_90_POS 45015900.0 90.0 10.0 POS
+
+    put pd2_fsb1_0_REFL f2 $x1
+    put pd2_fsb1_90_REFL f2 $x1
+    put pd2_fsb2_0_REFL f2 $x1
+    put pd2_fsb2_90_REFL f2 $x1
+    
+    put pd2_fsb1_0_AS f2 $x1
+    put pd2_fsb1_90_AS f2 $x1
+    put pd2_fsb2_0_AS f2 $x1
+    put pd2_fsb2_90_AS f2 $x1
+    
+    put pd2_fsb1_0_TMSX f2 $x1
+    put pd2_fsb1_90_TMSX f2 $x1
+    put pd2_fsb2_0_TMSX f2 $x1
+    put pd2_fsb2_90_TMSX f2 $x1
+    
+    put pd2_fsb1_0_TMSY f2 $x1
+    put pd2_fsb1_90_TMSY f2 $x1
+    put pd2_fsb2_0_TMSY f2 $x1
+    put pd2_fsb2_90_TMSY f2 $x1
+
+    put pd2_fsb1_0_POP f2 $x1
+    put pd2_fsb1_90_POP f2 $x1
+    put pd2_fsb2_0_POP f2 $x1
+    put pd2_fsb2_90_POP f2 $x1
+
+    put pd2_fsb1_0_POS f2 $x1
+    put pd2_fsb1_90_POS f2 $x1
+    put pd2_fsb2_0_POS f2 $x1
+    put pd2_fsb2_90_POS f2 $x1
+
+    yaxis log abs:deg
+    """)
+
+###### choosing DoF ######
+
+    if DoF == "CARM":
+        model.parse("""
+        xaxis sig1 f log 0.0005 500 1000
+        fsig sig1 ETMX phase 10.0 0.0 
+        fsig sig1 ETMY phase 10.0 0.0 
+        """)
+    elif DoF == "DARM":
+        model.parse("""
+        xaxis sig1 f log 0.0005 500 1000
+        fsig sig1 ETMX phase 10.0 0.0 
+        fsig sig1 ETMY phase 10.0 180.0 
+        """)
+    elif DoF == "MICH":
+        model.parse("""
+        xaxis sig1 f log 0.0005 500 1000
+        fsig sig1 ETMX phase 10.0 0.0 
+        fsig sig1 ITMX phase 10.0 0.0 
+        fsig sig1 ETMY phase 10.0 180.0 
+        fsig sig1 ITMY phase 10.0 180.0 
+        """)
+    elif DoF == "PRCL":
+        model.parse("""
+        xaxis sig1 f log 0.0005 500 1000
+        fsig sig1 PRM phase 1.0 0.0 
+        """)
+    elif DoF == "SRCL":
+        model.parse("""
+        xaxis sig1 f log 0.0005 500 1000
+        fsig sig1 SRM phase 1.0 0.0 
+        """)
+
+    else:
+        print("Please specify a valid DoF")
+    
+    
+    out = model.run()
+    
+    return model, out
+
+
+# In[ ]:
+
+
+
+
+
+# %%
